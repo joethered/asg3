aceed529f435834761c4b6932b4579e7701cb47a
Joe Wise <joethehobo2006@gmail.com>
2014-05-09 01:36:21 -0700

    implemented first stages of input reading. comments and regular key=value inputs.

--- main.cpp
+++ main.cpp
@@ -5,9 +5,10 @@
 #include <iostream>
 #include <string>
 #include <unistd.h>
+#include <fstream>
 
 //TODO delete this later
-#include <stdio.h>
+//#include <stdio.h>
 
 using namespace std;
 
@@ -18,11 +19,13 @@ using namespace std;
 typedef xpair<string,string> str_str_pair;
 typedef listmap<string,string> str_str_map;
 
+
 void scan_options (int argc, char** argv) {
+
    opterr = 0;
    for (;;) {
       int option = getopt (argc, argv, "@:");
-      if (option == EOF) break;
+      if (option == -1) break;
       switch (option) {
          case '@':
             traceflags::setflags (optarg);
@@ -35,15 +38,55 @@ void scan_options (int argc, char** argv) {
    }
 }
 
+
 int main (int argc, char** argv) {
    sys_info::set_execname (argv[0]);
    scan_options (argc, argv);
 
    str_str_map test;
    for (int argi = 0; argi < argc; ++argi) {
-      str_str_pair pair (argv[argi], to_string<int> (argi));
-      cout << "Before insert: " << pair << endl;
-      test.insert (pair);
+
+      //line will be the current line from the file.
+      string line;
+      ifstream myfile (argv[argi]);
+      if (myfile.is_open())
+      {
+            //reads in from the file line by line
+            while ( getline (myfile,line) )
+            {
+                //if the first char is a # it's a comment and is ignored
+                if(line[0]=='#')
+                    continue;
+
+                //found_white_space holds position of first instance of whitespace
+                size_t found_white_space = line.find_first_of(" ");
+
+                //goes through the line and erases all whitespace
+                while(found_white_space!=std::string::npos)
+                {
+                    line.erase(found_white_space, 1);
+                    found_white_space = line.find_first_of(" ");
+                }
+
+                //found holds position of the first equals sign
+                size_t found = line.find_first_of("=");
+
+                //split up line around the '=' to make two strings, key and value
+                string key = line.substr(0,found-1);
+                string value = line.substr(found+1);
+
+                //construct a new pair using key and value and insert it into the map
+                str_str_pair insert_pair (key, value);
+                test.insert (insert_pair);
+
+            }
+
+            myfile.close();
+      }
+
+  else cout << "Unable to open file";
+
+
    }
 
    for (str_str_map::iterator itor = test.begin();
